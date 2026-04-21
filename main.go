e552525f20827b3f0aab32cc5ba1b17115a3af00
raraend <raraevamaharani@gmail.com>
2026-04-21 18:14:59 +0900

    First Commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+import(
+	"fmt"
+)
+
+func CsvToSlice(data string) (map[string][]string, error) {
+} 
+
+
+func (connector *AIModelConnector) ConnectAIModel(payload Inputs, token string) (Response, error) {
+}
+
+func main(){
+
+}
